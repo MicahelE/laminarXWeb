5805b950cbc50839528ee2572d151533b2eb3e22
72313832 <72313832@users.noreply.github.com>
2023-10-20 20:35:22 -0400

    added Docker config

--- resources/js/components/Home.jsx
+++ resources/js/components/Home.jsx
@@ -1,42 +1,42 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import  {Link}  from 'react-router-dom';
-import {Example} from './Example';
-import { Button } from "@/Components/ui/button";
-import { Label } from "@/Components/ui/label";
-import  Page  from "./Page"
-import  CustomFormValidation  from "./CustomFormValidation"
-import  Login  from "./Login"
-import  Template  from "./Template"
+import { Link } from 'react-router-dom';
+import { Example } from './Example';
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import Page from "./Page"
+import CustomFormValidation from "./CustomFormValidation"
+import Login from "./Login"
+import Template from "./Template"
 // import Link from '@mui/material/Link';
-import  SignInSide  from "./SignInSide"
-import  SignInTwo  from "./SignInTwo"
-import  PageThree  from "./PageThree"
-import  PageFour  from "./PageFour"
-import  PageFive  from "./PageFive"
-import  PageSix  from "./PageSix"
-import  PageSeven  from "./PageSeven"
-import  PageEight  from "./PageEight"
+import SignInSide from "./SignInSide"
+import SignInTwo from "./SignInTwo"
+import PageThree from "./PageThree"
+import PageFour from "./PageFour"
+import PageFive from "./PageFive"
+import PageSix from "./PageSix"
+import PageSeven from "./PageSeven"
+import PageEight from "./PageEight"
 
 
 export default function Home() {
     const heading = "Laminar X Web change";
-        return (  
-            <div> 
-               
+    return (
+        <div>
+
             <BrowserRouter>
-         
-<Routes>
-    <Route path="/" element={<SignInSide/>}/>
-    <Route path="/log" element={<Login/>}/>
-    <Route path="/two" element={<SignInTwo/>}/>
-    <Route path="/three" element={<PageThree/>}/>
-    <Route path="/four" element={<PageFour/>}/>
-    <Route path="/five" element={<PageFive/>}/>
-    <Route path="/six" element={<PageSix/>}/>
-    <Route path="/seven" element={<PageSeven/>}/>
-    <Route path="/eight" element={<PageEight/>}/>
-</Routes>
-</BrowserRouter>
-            </div>
-         );
+
+                <Routes>
+                    <Route path="/" element={<SignInSide />} />
+                    <Route path="/log" element={<Login />} />
+                    <Route path="/two" element={<SignInTwo />} />
+                    <Route path="/three" element={<PageThree />} />
+                    <Route path="/four" element={<PageFour />} />
+                    <Route path="/five" element={<PageFive />} />
+                    <Route path="/six" element={<PageSix />} />
+                    <Route path="/seven" element={<PageSeven />} />
+                    <Route path="/eight" element={<PageEight />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,17 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    resolve: {
+        alias: {
+            src: 'resources/img',
+        },
+    },
     plugins: [
         laravel({
             input: [
                 'resources/sass/app.scss',
-                'resources/js/app.js',
+                'resources/css/app.css',
+                'resources/js/app.jsx'
             ],
             refresh: true,
         }),
